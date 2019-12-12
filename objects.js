1fa65481e485a9e87ca83c9a16b0f801eb6046b5
EspiDev <espidev@gmail.com>
2019-12-12 09:28:19 -0500

    init

---
+++ objects.js
@@ -0,0 +1,18 @@
+
+
+
+class MovingObject {
+    constructor (name, mass, posX, posY) {
+        this.name = name;
+        this.mass = mass;
+        this.posX = posX;
+        this.posY = posY;
+        this.velX = 0;
+        this.velY = 0;
+        this.accelX = 0;
+        this.accelY = 0;
+    }
+
+
+}
+
