810486ee9a473e8614c853ff49567e1ecd5d1554
EspiDev <espidev@gmail.com>
2019-12-14 19:24:36 -0500

    weird

--- objects.js
+++ objects.js
@@ -1,23 +1,11 @@
-
-class MovingObject {
-
-    // all units in SI
-    constructor() {
-        this.name = "";
-        this.mass = 0.0;
-        this.posX = 0.0;
-        this.posY = 0.0;
-        this.velX = 0.0;
-        this.velY = 0.0;
-        this.accelX = 0.0;
-        this.accelY = 0.0;
-    }
-
-}
-
+let speed = 24*3600/50;
 let movingObjects = [];
-
 let GRAVITATIONAL_CONSTANT = 6.67408e-11;
+let fps = 60;
+
+const AU = (149.6e6 * 1000);
+let scalevalue = 50;
+let SCALE = scalevalue / AU;
 
 function distBetweenTwoObj(objectA, objectB) {
     return Math.sqrt((objectB.posX-objectA.posX)*(objectB.posX-objectA.posX)+(objectB.posY-objectA.posY)*(objectB.posY-objectA.posY));
@@ -36,51 +24,90 @@ function angleBetweenTwoObj(objectA, objectB) {
     return Math.atan(deltaY-deltaX);
 }
 
+class MovingObject {
+
+    // all units in SI
+    constructor() {
+        this.id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        this.mass = 3e+24;
+        this.posX = 0.0;
+        this.posY = 0.0;
+        this.velX = 0.0;
+        this.velY = 0.0;
+        this.accelX = 0.0;
+        this.accelY = 0.0;
+        this.color = "#FFFFFF";
+    }
+
+    calcAccel() {
+        this.accelX = 0;
+        this.accelY = 0;
+        movingObjects.forEach(obj => {
+            if (obj !== this) {
+                let force = forceBetweenTwoObj(this, obj);
+                let rad = angleBetweenTwoObj(this, obj);
+
+                // use minus to reverse vector
+                this.accelX -= (force * Math.cos(rad)) / this.mass;
+                this.accelY -= (force * Math.sin(rad)) / this.mass;
+            }
+        });
+    }
+
+    calcVelocity() {
+        this.velX += this.accelX * speed;
+        this.velY += this.accelY * speed;
+        this.calcPosition();
+    }
+
+    calcPosition() {
+        this.posX += this.velX * speed;
+        this.posY += this.velY * speed;
+    }
+
+    draw (ctx) {
+        ctx.lineWidth = 3;
+        ctx.fillStyle = this.color;
+
+        ctx.beginPath();
+        ctx.arc(this.posX*SCALE, this.posY*SCALE, 3, 0, 2*Math.PI, false);
+        ctx.fill();
+    }
+}
+
+let sun = new MovingObject({
+    id: "sun",
+    mass: 1.989e30,
+    posX: 0,
+    posY: 0,
+    velX: 0,
+    velY: 0,
+    accelX: 0,
+    accelY: 0,
+    color: "yellow",
+});
+
+let earth = new MovingObject({
+    id: "earth",
+    mass: 5.972e24,
+    posX: -147.09e6*1000,
+    posY: 0,
+    velX: 30290,
+    velY: 0,
+    color: "blue",
+});
+
 function createMovingObject() {
-    movingObjects.push(new MovingObject());
+    let obj = new MovingObject();
+    movingObjects.push(obj);
+    return obj;
 }
 
-// calculate motion
-function calcTimeFrame(seconds) {
-    for (let i = 0; i < seconds; i++) {
-        calcForce();
-        calcVelocity(1);
-        calcDisplacement(1);
-    }
+
+function frames() {
+    setTimeout(function() {
+
+    }, 1000/60);
 }
 
-function calcDisplacement(seconds) {
-    for (let i = 0; i < movingObjects.length; i++) {
-        let objA = movingObjects[i];
-        objA.posX += objA.velX * seconds;
-        objA.posY += objA.velY * seconds;
-    }
-}
-
-function calcVelocity(seconds) {
-    for (let i = 0; i < movingObjects.length; i++) {
-        let objA = movingObjects[i];
-        objA.velX += objA.accelX * seconds;
-        objA.velY += objA.accelY * seconds;
-    }
-}
-
-// calculates acceleration, should technically be called for every movement but the computer is not fast enough
-function calcForce() {
-    for (let i = 0; i < movingObjects.length; i++) {
-        let objA = movingObjects[i];
-        objA.accelX = 0;
-        objA.accelY = 0;
-        for (let j = 0; j < movingObjects; j++) {
-            if (i === j) continue;
-
-            let objB = movingObjects[j];
-            let force = forceBetweenTwoObj(objA, objB);
-            let rad = angleBetweenTwoObj(objA, objB);
-
-            // use minus to reverse vector
-            objA.accelX -= (force * Math.cos(rad)) / objA.mass;
-            objA.accelY -= (force * Math.sin(rad)) / objA.mass;
-        }
-    }
-}
+frames();
--- sidebar.js
+++ sidebar.js
@@ -1,3 +1,29 @@
-function onCreateCard() {
 
+
+function onCreateCard() {
+    obj = createMovingObject();
+    $("#cards").append(`
+    <div class="card sidebar-elem">
+        <b>` + obj.id + `</b>
+        <div>Mass: <input type="number" id="` + obj.id + `-mass" value="` + obj.mass + `"> kg</div>
+        <div>PosX: <input type="number" id="` + obj.id + `-posx" value="` + obj.posX + `"></div>
+        <div>PosY: <input type="number" id="` + obj.id + `-posy" value="` + obj.posY + `"></div>
+        <div>VelX: <input type="number" id="` + obj.id + `-velx" value="` + obj.velX + `"> m/s</div>
+        <div>VelY: <input type="number" id="` + obj.id + `-vely" value="` + obj.velY + `"> m/s</div>
+        <div>AccelX: <input type="number" id="` + obj.id + `-accelx" value="` + obj.accelX + `"> m/s2</div>
+        <div>AccelY: <input type="number" id="` + obj.id + `-accely" value="` + obj.accelY + `"> m/s2</div>
+    </div>
+    `);
+}
+
+function updateCards() {
+    movingObjects.forEach(obj => {
+        $("#" + obj.id + "-mass").val(obj.mass);
+        $("#" + obj.id + "-posx").val(obj.posX);
+        $("#" + obj.id + "-posy").val(obj.posY);
+        $("#" + obj.id + "-velx").val(obj.velX);
+        $("#" + obj.id + "-vely").val(obj.velY);
+        $("#" + obj.id + "-accelx").val(obj.accelX);
+        $("#" + obj.id + "-accely").val(obj.accelY);
+    });
 }
